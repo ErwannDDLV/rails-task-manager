6ade5a50af67b1c140a9c67fcb21821c31eefe8c
Erwann de Dieuleveult <ededieuleveult@gmail.com>
2017-02-14 17:23:50 +0100

    a new tasks manager to manage with some CSS

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,24 @@
 Rails.application.routes.draw do
-  get    "tasks",          to: "tasks#index"
-
-  get    "tasks/new",      to: "tasks#new", as: :new
-  post   "tasks",          to: "tasks#create", as: :create
-
-  get    "tasks/:id",      to: "tasks#show"
-
-  get    "tasks/:id/edit", to: "tasks#edit", as: :edit
-  patch  "tasks/:id",      to: "tasks#update", as: :update
-
-  delete "tasks/:id",      to: "tasks#destroy", as: :destroy
+  get "tasks/index", to: "tasks#index"
+  resources :tasks
 end
+
+
+# Rails.application.routes.draw do
+#   resources :restaurants
+# end
+
+# juste pour voir certaines pages :
+# resources :restaurants, only: [:index, :show]
+
+  # get    "tasks",          to: "tasks#index"
+
+  # get    "tasks/new",      to: "tasks#new", as: :new
+  # post   "tasks",          to: "tasks#create", as: :create
+
+  # get    "tasks/:id",      to: "tasks#show"
+
+  # get    "tasks/:id/edit", to: "tasks#edit", as: :edit
+  # patch  "tasks/:id",      to: "tasks#update", as: :update
+
+  # delete "tasks/:id",      to: "tasks#destroy", as: :destroy
